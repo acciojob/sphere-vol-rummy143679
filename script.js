a340074d44a41e962c685562fa616cc623678470
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-20 00:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
+	const rie = document.getElementById('radius');
+	const vie = document.getElementById('volume');
+
+	if(parseInt(rie.value) > 0){
+		const result = (4/3)* Math.PI*Math.pow(rie.value, 3);
+		return result.toFixed(4);
+	}else{
+		retunr parseInt(rie.value);
+	}
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
